d142b7dc06f5141f2c37178f478ab52af1017e49
Seongwon20 <seangwon20@gmail.com>
2025-05-12 13:46:00 +0900

    ✨feat: 시간표에 들어갈 과목들 classInfo에 추가

---
+++ team/src/components/attendance/ClassInfo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const classList = [
+  {
+    name: "UX/UI 지적 재산권 [A]",
+    professor: "김교수",
+    startTime: "10:30",
+    endTime: "12:00",
+    location: "탐구관 502호"
+  },
+  {
+    name: "AI와 HCI [D]",
+    professor: "김교수",
+    startTime: "13:30",
+    endTime: "15:00",
+    location: "탐구관 508호"
+  },
+  {
+    name: "영어 커뮤니케이션 [B]",
+    professor: "황교수",
+    startTime: "16:00",
+    endTime: "19:00",
+    location: "탐구관 502호"
+  }
+];
