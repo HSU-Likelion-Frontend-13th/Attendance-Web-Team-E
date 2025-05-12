a04c30b3d0ef1ff72a9bd952d9308e80e15b171b
Seongwon20 <seangwon20@gmail.com>
2025-05-12 13:31:01 +0900

    ✨feat: AttendancePage에 import 6개 추가

---
+++ team/src/pages/attendance/AttendancePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AttendanceButton from "../../components/attendance/AttendanceButton";
+import CurrentTime from "../../components/attendance/CurrentTime";
+import ClassInfo from "../../components/attendance/ClassInfo";
+import StatusTitle from "../../components/attendance/StatusTitle";
+import StatusIcon from "../../components/attendance/StatusIcon";
+import StatusDescription from "../../components/attendance/StatusDescription";
+
+
+
+
+
+export default function AttendancePage() {
+  return (
+    <div>
+      <h1>AttendancePage 페이지 입니다.</h1>
+    </div>
+  );
+}
