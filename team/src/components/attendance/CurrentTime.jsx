2193aa863d4a4bb3cb4cea0c0bb019e7d56e3fa9
Seongwon20 <seangwon20@gmail.com>
2025-05-13 10:35:41 +0900

    🔥 clean: 현재 날짜: 이라는 문구 삭

--- team/src/components/attendance/CurrentTime.jsx
+++ team/src/components/attendance/CurrentTime.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function CurrentTime({ now }) { //now 값을 props로 받아옴
   return (
     <div>
-      <h3>현재 날짜: {now.toLocaleDateString()} {now.toLocaleTimeString()}</h3>
+      <h3>{now.toLocaleDateString()} {now.toLocaleTimeString()}</h3>
       
       
     </div>
