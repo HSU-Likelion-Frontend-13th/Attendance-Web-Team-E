2f6d3f2980d3272788011413948714177133577d
nyun-nye <2001_11_7@naver.com>
2025-05-13 17:22:54 +0900

    ✨: 다예 변경사항 반영

--- team/src/App.test.js
+++ team/src/App.test.js
@@ -1,19 +1,8 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
-import GlobalStyles from './GlobalStyles'; // 글로벌 스타일 import
-import TableItem from './pages/timetable/TableItem'; // TableItem 컴포넌트 import
-
 function App() {
-  return (
-    <div className="App">
-      <Link to="/login">Login</Link>
-        
-        <GlobalStyles /> {/* 글로벌 스타일 적용 */}
-      <TableItem />
-      {/* Add your routes here */}
-    </div>
-  );
+  return <div className="App">{/* Add your routes here */}</div>;
 }
 
 export default App;
